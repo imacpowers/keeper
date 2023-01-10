91559d80a921b8352e67f89aef74fff9c104cfe7
Andrew <odoleandrew@yahoo.com>
2023-01-10 17:24:01 +0100

    first commit

---
+++ build/precache-manifest.4f3d6ced7ea530411748ff027001e51d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b0a0fb7094319fecfad5caa040766d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "140303718ebb369fd3fc",
+    "url": "/static/js/2.e928da4b.chunk.js"
+  },
+  {
+    "revision": "dbbd8c242eebe58534d9",
+    "url": "/static/js/main.5668e9a1.chunk.js"
+  },
+  {
+    "revision": "2f55955791534fe0d08b",
+    "url": "/static/js/runtime-main.1048f588.js"
+  }
+]);
