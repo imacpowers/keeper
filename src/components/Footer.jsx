d184350db68832d92a056fa8a6065ca37048092f
Andrew <odoleandrew@yahoo.com>
2023-01-10 11:58:05 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ 2022</p>
+    </footer>
+  );
+}
+
+export default Footer;
